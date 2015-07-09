19dfc2e456314ebf1a69337f8a80f68c388ab1c5
Leigh McCulloch <leigh@mcchouse.com>
2015-07-08 21:39:51 -0700

    Changed tests from ES5 => ES6 module syntax.

--- test/acceptedlanguages-user.test.js
+++ test/acceptedlanguages-user.test.js
@@ -1,58 +1,54 @@
-define([
-  'acceptedlanguages-root',
-  'acceptedlanguages-user'
-], function(root, user){
+import * as root from 'acceptedlanguages-root';
+import * as user from 'acceptedlanguages-user';
 
-  var mockRoot = {};
+var mockRoot = {};
 
-  beforeEach(function() {
-    root.setRoot(mockRoot);
-  });
+beforeEach(function() {
+  root.setRoot(mockRoot);
+});
 
-  afterEach(function() {
-    root.setRoot(this);
-  });
+afterEach(function() {
+  root.setRoot(this);
+});
 
-  describe('acceptedlanguages-util', function() {
+describe('acceptedlanguages-util', function() {
 
-    describe('.accepted', function() {
+  describe('.accepted', function() {
 
-      describe('navigator defines languages', function() {
-        beforeEach(function() {
-          mockRoot.navigator = {
-            languages: ['en-AU', 'en', 'jp']
-          }
-        });
-
-        it('should return the browsers languages', function() {
-          expect(user.getAcceptedLanguages()).toEqual(['en-AU', 'en', 'jp']);
-        });
+    describe('navigator defines languages', function() {
+      beforeEach(function() {
+        mockRoot.navigator = {
+          languages: ['en-AU', 'en', 'jp']
+        }
       });
 
-      describe('navigator defines language', function() {
-        beforeEach(function() {
-          mockRoot.navigator = {
-            language: 'en-AU'
-          }
-        });
+      it('should return the browsers languages', function() {
+        expect(user.getAcceptedLanguages()).toEqual(['en-AU', 'en', 'jp']);
+      });
+    });
 
-        it('should return the browsers languages', function() {
-          expect(user.getAcceptedLanguages()).toEqual(['en-AU']);
-        });
+    describe('navigator defines language', function() {
+      beforeEach(function() {
+        mockRoot.navigator = {
+          language: 'en-AU'
+        }
       });
 
-      describe('navigator defines userLanguage', function() {
-        beforeEach(function() {
-          mockRoot.navigator = {
-            userLanguage: 'en-AU'
-          }
-        });
+      it('should return the browsers languages', function() {
+        expect(user.getAcceptedLanguages()).toEqual(['en-AU']);
+      });
+    });
 
-        it('should return the browsers languages', function() {
-          expect(user.getAcceptedLanguages()).toEqual(['en-AU']);
-        });
+    describe('navigator defines userLanguage', function() {
+      beforeEach(function() {
+        mockRoot.navigator = {
+          userLanguage: 'en-AU'
+        }
       });
 
+      it('should return the browsers languages', function() {
+        expect(user.getAcceptedLanguages()).toEqual(['en-AU']);
+      });
     });
 
   });
